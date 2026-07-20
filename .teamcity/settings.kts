9282d4878695cc4f06b3a2df67ae071248124857
admin <admin@TeamCity>
2026-07-20 10:41:09 +0000

    triggers of 'Build' build configuration were updated (TeamCity change in 'vfdfs' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.vcsLabeling
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -61,6 +62,11 @@ object Build : BuildType({
                 doesNotEqual("teamcity.build.branch", "master")
             }
             goals = "clean test"
+        }
+    }
+
+    triggers {
+        vcs {
         }
     }
 
